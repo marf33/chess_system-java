4d5a6c53c61f64ee6c56e944c86868232fb3038d
Miguel Figueiredo <miguel.figueiredo96@gmail.com>
2024-02-22 10:30:13 +0000

    CheckMate

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -3,7 +3,6 @@ package chess;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
